14e8f27556f7d53cae4dfd0ca6205a9cbf92c8a7
Alexander Lopatin <alopatindev@gmail.com>
2017-02-09 01:17:01 +0300

    format message, better error handling

--- app.js
+++ app.js
@@ -6,26 +6,28 @@ const logger = require('./logger.js')(config)
 const SearchEngines = require('./search-engines.js')
 const Telegraf = require('telegraf')
 
-const WELCOME_MESSAGE = 'Hi! Please type your search query!'
+const TEXT_WELCOME = 'Hi! Please type your search query!'
 
 const searchEngines = new SearchEngines(logger)
 const bot = new Telegraf(config.telegram.botToken)
 
 bot.command('start', ctx => {
-    ctx.reply(WELCOME_MESSAGE)
+    ctx.reply(TEXT_WELCOME).catch(logger.error)
 })
 
 bot.on('message', ctx => {
     const query = ctx.message.text.trim()
+
     if (query.length === 0) {
-        ctx.reply(WELCOME_MESSAGE)
+        ctx.reply(TEXT_WELCOME).catch(logger.error)
     } else {
         searchEngines
-            .run(query)
+            .find(query)
             .then(result => {
-                ctx.reply(result)
+                logger.debug(result)
+                return ctx.reply(result)
             })
-            .catch(err => logger.error(err))
+            .catch(logger.error)
     }
 })
 
--- search-engines.js
+++ search-engines.js
@@ -1,14 +1,46 @@
 'use strict'
 
+const phantom = require('phantom')
+const concatMaps = require('concat-maps')
+
+const TEXT_FOUND_BOTS = 'Found bots: '
+
 class SearchEngines {
     constructor(logger) {
         this.logger = logger
     }
 
-    run(query) {
-        return new Promise((resolve, reject) => {
-            this.logger.debug(`running query ${query}`)
-            resolve(`result of ${query}`)
+    find(query) {
+        // TODO: if cached then get
+        return Promise
+            .all([this.findInBotList(query), this.findInStoreBot(query)])
+            .then(results => {
+                const mergedResults = concatMaps.concat(...results)
+                const text = Array
+                    .from(mergedResults)
+                    .map(([bot, description]) => `@${bot} — ${description}`)
+                    .join('\n')
+
+                return `${TEXT_FOUND_BOTS}${mergedResults.size}\n${text}`
+            })
+            .catch(this.logger.error)
+    }
+
+    findInBotList(query) {
+        return new Promise(resolve => {
+            const results = new Map([
+                ['test2', 'teeest'],
+            ])
+            resolve(results)
+        })
+    }
+
+    findInStoreBot(query) {
+        return new Promise(resolve => {
+            const results = new Map([
+                ['test1', 'teeest'],
+            ])
+            resolve(results)
         })
     }
 }
