35a7def575dc187909597863715becb46938bafc
Alexander Lopatin <alopatindev@gmail.com>
2017-02-09 16:09:30 +0300

    use config.js instead of config.json

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const config = require('./config.json')
+const config = require('./config.js')
 const logger = require('./logger.js')(config)
 
 const SearchEngines = require('./search-engines.js')
@@ -9,7 +9,7 @@ const Telegraf = require('telegraf')
 const TEXT_WELCOME = 'Hi! Please type your search query!'
 
 const searchEngines = new SearchEngines(logger)
-const bot = new Telegraf(config.telegram.botToken)
+const bot = new Telegraf(config.telegramBotToken)
 
 bot.command('start', ctx => {
     ctx.reply(TEXT_WELCOME).catch(logger.error)
