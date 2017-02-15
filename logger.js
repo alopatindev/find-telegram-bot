25806cb6ef3e877a80d63bb3382ea52f38dae658
Alexander Lopatin <alopatindev@gmail.com>
2017-02-16 02:50:25 +0300

    add license notice

--- logger.js
+++ logger.js
@@ -1,3 +1,10 @@
+/*!
+ * find-telegram-bot <https://github.com/alopatindev/find-telegram-bot>
+ *
+ * Copyright (c) 2017 Alexander Lopatin
+ * Licensed under the MIT License
+ */
+
 'use strict'
 
 const winston = require('winston')
