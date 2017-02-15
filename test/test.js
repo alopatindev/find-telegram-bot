79af14f9e1cf754433adc967d270a2e2d390f02d
Alexander Lopatin <alopatindev@gmail.com>
2017-02-15 22:52:01 +0300

    testing Scrapers.find

--- test/test.js
+++ test/test.js
@@ -2,10 +2,55 @@
 
 const assert = require('assert')
 
-describe('Array', () => {
-    describe('#indexOf()', () => {
-        it('should return -1 when the value is not present', () => {
-            assert.equal(-1, [1, 2, 3].indexOf(4))
+const Scrapers = require('../scrapers')
+
+describe('Scrapers', () => {
+    const stubFunction = () => undefined
+    const appObjectsMock = {
+        config: {
+            scrapers: ['scraperA', 'scraperB'],
+        },
+        logger: {
+            debug: stubFunction,
+            info: stubFunction,
+        },
+    }
+
+    function onCreatePageEmptyMock(query, phantomObjects) {
+        return new Promise(resolve => {
+            phantomObjects
+                .instancePromise
+                .then(instance => instance.exit())
+            resolve(new Map())
         })
+    }
+
+    describe('#find()', () => {
+        it('should merge empty maps', done => {
+            const createPageCallbacks = {
+                scraperA: onCreatePageEmptyMock,
+                scraperB: onCreatePageEmptyMock,
+            }
+
+            const scrapers = new Scrapers(appObjectsMock, createPageCallbacks)
+            scrapers
+                .find('hello')
+                .then(results => assert.equal(results, ''))
+                .then(done)
+        })
+
+        it('should sort by bot names', () => {})
+
+        it('should ignore bots names case-sensitivity', () => {})
+
+        it('should override previous descriptions', () => {})
+
+        it('should trim description whitespace', () => {})
+
+        it('should remove URLs from description', () => {})
+
+        it('should remove @ from description', () => {})
+
+        it('should remove new lines from description', () => {})
     })
 })
