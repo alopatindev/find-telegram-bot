7293927b0c5cdda0597fa76a632ac2d005401486
Alexander Lopatin <alopatindev@gmail.com>
2017-02-18 03:48:04 +0300

    use a separate Dockerfile for debugging

--- debug-container.sh
+++ debug-container.sh
@@ -4,21 +4,25 @@ set -euo pipefail
 IFS=$'\n\t'
 
 NAME="$(basename $(pwd))"
+DOCKERFILE="Dockerfile.debug"
 
 stop() {
     echo 'stopping'
     docker stop "${NAME}" || echo 'no container'
     docker rm -v "${NAME}" || echo 'no container'
     docker image rm "${NAME}" || echo 'no image'
+    #docker rm $(docker ps -q -f status=exited) || echo 'nothing to remove'
 }
 
 start() {
     stop
+    rm -rf node_modules
 
     echo 'starting'
-    docker build -t "${NAME}" .
-    docker run --name "${NAME}" -v "${HOME}/docker/${NAME}:/app/logs" -d "${NAME}"
-    docker stats "${NAME}"
+    docker build -t "${NAME}" -f "${DOCKERFILE}" .
+    docker run --name "${NAME}" -v "${HOME}/docker/${NAME}:/app/logs" "${NAME}"
+    #docker run --name "${NAME}" --restart=on-failure -d -v "${HOME}/docker/${NAME}:/app/logs" "${NAME}"
+    #docker stats "${NAME}"
 }
 
 if [ $# -ne 1 ]; then
